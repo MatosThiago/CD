ab34b7df93d1d6e45fd5729be72d478ae711c34a
Thiago Matos <thiago.matos882@gmail.com>
2018-06-04 22:51:22 -0300

    Pré Projeto

--- Calculadora/simulation/qsim/ULA.do
+++ Calculadora/simulation/qsim/ULA.do
@@ -2,9 +2,9 @@ onerror {quit -f}
 vlib work
 vlog -work work ULA.vo
 vlog -work work ULA.vt
-vsim -novopt -c -t 1ps -L cycloneii_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate work.mux2_vlg_vec_tst
+vsim -novopt -c -t 1ps -L cycloneii_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate work.4Bit_Multiplier_vlg_vec_tst
 vcd file -direction ULA.msim.vcd
-vcd add -internal mux2_vlg_vec_tst/*
-vcd add -internal mux2_vlg_vec_tst/i1/*
+vcd add -internal 4Bit_Multiplier_vlg_vec_tst/*
+vcd add -internal 4Bit_Multiplier_vlg_vec_tst/i1/*
 add wave /*
 run -all
---
+++ Calculadora/simulation/qsim/work/@1344@bit_@multiplier@040/_primary.vhd
@@ -0,0 +1,22 @@
+library verilog;
+use verilog.vl_types.all;
+entity \4Bit_Multiplier\ is
+    port(
+        P0              : out    vl_logic;
+        B0              : in     vl_logic;
+        A0              : in     vl_logic;
+        P1              : out    vl_logic;
+        B1              : in     vl_logic;
+        A1              : in     vl_logic;
+        P2              : out    vl_logic;
+        A2              : in     vl_logic;
+        B2              : in     vl_logic;
+        P3              : out    vl_logic;
+        A3              : in     vl_logic;
+        B3              : in     vl_logic;
+        P4              : out    vl_logic;
+        P5              : out    vl_logic;
+        P6              : out    vl_logic;
+        P7              : out    vl_logic
+    );
+end \4Bit_Multiplier\;
